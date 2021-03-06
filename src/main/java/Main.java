8f0c2650db0e10cec1c2d6670fa4c54215ea5847
Ismael Felix <ismael.duarte017@gmail.com>
2021-03-06 17:28:19 -0300

    Camada de Xadrez e imprimindo tabuleiro

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import board.Position;
+import chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args){
-        Position position = new Position(3,5);
 
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
 
     }
 }
